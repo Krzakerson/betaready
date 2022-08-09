2244932870724e90e5bc8ae85218579cdd3949d5
Krzakerson <92225516+Krzakerson@users.noreply.github.com>
2022-08-09 09:37:53 +0200

    tests

--- Michal/TestProject1/UnitTest1.cs
+++ Michal/TestProject1/UnitTest1.cs
@@ -3,14 +3,44 @@ using Api1;
 using WebApplication1.Models;
 using RestSharp;
 using Newtonsoft.Json;
+using System.Net.Http;
+using Microsoft.AspNetCore.Mvc.Testing;
+using ikvm.runtime;
+using System.Net;
 
 namespace TestProject1
 {
     [TestClass]
     public class UnitTest1
     {
+        private static WebApplicationFactory<Startup> _factory;
+
+        /*[Fact]
+
+      public async Task GetPracownicy()
+      {
+          await using var application = new WebApplicationFactory<Api.Startup>();
+          using var client = application.CreateClient();
+          var response = await client.GetAsync("http://localhost:5100/api/Pracownik/");
+          response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+      }
+      
         [TestMethod]
-        public void TestMethod1()
+
+        public async Task ShouldReturnSucces()
+        {
+            var client = _factory.CreateClient();
+            var response = await client.GetAsync("api/values");
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            Assert.AreEqual("application/json; charset=utf-8", response.Content.Headers.ContentType?.ToString());
+
+            var json = await response.Content.ReadAsStringAsync();
+            Assert.AreEqual("[\"value1\",\"value2\"]", json);
+        }
+        */
+        [TestMethod]
+        public void TestMethodPost()
         {
             Pracownik pracownik = new Pracownik()
 
@@ -27,29 +57,32 @@ namespace TestProject1
            
             List<Pracownik> pracowniks = new List<Pracownik>();
             var _client = new RestClient();
-            var request = new RestRequest("http://localhost:5100/api/Pracownik/", Method.Put);
+            var request = new RestRequest("http://localhost:5100/api/Pracownik/", Method.Post);
             //JsonConvert.PopulateObject(values, temp);
             request.AddParameter("application/json",  ParameterType.RequestBody);
             request.AddHeader("Content-Type", "application/json");
             var response = _client.Execute(request);
             Assert.IsNotNull(response);
-            Assert.AreEqual(200, response.StatusCode);
+           //Assert.AreEqual(200, response.StatusCode);  
+            //Assert.AreEqual(System.Net.HttpStatusCode.OK,response.Content);
            
         }
+ 
 
-        [TestMethod]
-
-        public void TestMethodPost()
-        {
-            
-        }
+       
 
 
         [TestMethod]
 
-        public void TestMethodDelete()
+        public void TestMethodDelete(int key)
         {
-
+            var _client = new RestClient();
+            var request = new RestRequest($"http://localhost:5100/api/Pracownik/{key}", Method.Delete);
+            request.AddParameter("application/json", ParameterType.RequestBody);
+            request.AddHeader("Content-Type", "application/json");
+            var response =  _client.Execute(request);
+            Assert.IsNotNull(response);
+            Assert.AreEqual(System.Net.HttpStatusCode.OK, response.Content);
         }
 
 
